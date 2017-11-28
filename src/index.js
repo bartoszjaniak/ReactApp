2d50dde4ec78408e18177f0d6c7f126679498b53
bartoszjaniak <bartoszjaniak@outlook.com>
2017-11-28 23:02:51 +0100

    Dodanie projektu z kursu

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyD6_vwe4Cg16doHe4d7BziPMQVnqyF4O60';
+
+
+//create component 
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+
+//pur component into page 
+
+ReactDOM.render(<App />,document.querySelector('.container')); 
