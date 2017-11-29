6a7088035bf1b1ab1119656b4226901d612fb258
bartoszjaniak <bartoszjaniak@outlook.com>
2017-11-29 18:37:56 +0100

    Dodanie kosntruktora

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class SearchBar extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
         return <input onChange={ event => console.log(event.target.value) } />;    
     }
