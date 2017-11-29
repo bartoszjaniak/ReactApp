b1e2670bb9be28024d082d916b3de678d8a59012
bartoszjaniak <bartoszjaniak@outlook.com>
2017-11-29 23:43:44 +0100

    dodanie komponentu VideoList

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD6_vwe4Cg16doHe4d7BziPMQVnqyF4O60';
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-});
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state = { videos: [] }    
+        this.state = { videos: [] };
+
+
+        YTSearch({ key: API_KEY, term: 'surfboards' },  (videos)  => {
+            this.setState({ videos });
+            //same like : this.setState({ videos: videos});
+        });
     }
 
     render(){
@@ -22,8 +26,8 @@ class App extends Component {
             <div>
                 <SearchBar />
             </div>
-        )
+        );
     }
-};
+}
 
 ReactDOM.render(<App />,document.querySelector('.container')); 
