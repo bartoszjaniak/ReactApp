03272dac9d9a2e5f632b367796d8a2231a959372
bartoszjaniak <bartoszjaniak@outlook.com>
2017-11-29 23:35:43 +0100

    refaktoryzacja App i dodanie konstruktora

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 
 const API_KEY = 'AIzaSyD6_vwe4Cg16doHe4d7BziPMQVnqyF4O60';
 
 
-//create component 
-const App = () => {
-    return( 
-    <div>
-        <SearchBar />
-    </div>
-    )
-}
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+});
 
+class App extends Component {
+    constructor(props){
+        super(props);
 
-//pur component into page 
+        this.state = { videos: [] }    
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+};
 
 ReactDOM.render(<App />,document.querySelector('.container')); 
